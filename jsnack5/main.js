bfa6d2cf44797287df61fc959c02492968f78f86
Francesco-Iannuzzi <iannuzzi.francesco@outlook.com>
2023-03-24 12:48:41 +0100

    initial commit define variables

---
+++ jsnack5/main.js
@@ -0,0 +1,14 @@
+/*
+
+Jsnack5
+Scrivi una funzione che accetti tre argomenti:
+un array e due numeri ("a" più piccolo di "b" e "b" grande al massimo quanto il numero di elementi dell'array).
+La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra "a" e "b"
+
+*/
+
+const arrayElement = [ 1, 2, 3, 6, 8]
+//console.log(arrayElement.length);
+
+const numero1 = 2
+const numero2 = 5
